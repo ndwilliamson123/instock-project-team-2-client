a6501f7c79dc38fd11a1948cb67ceeb0020fe67c
Kevin Tran <kevtran@hotmail.com>
2022-10-08 15:14:23 -0700

    Address PR comments

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,10 @@ export default function Footer() {
     const copyrightMessage = "InStock Inc. All Rights Reserved.";
 
     return (
-        <>
-            <footer className="footer">
-                <div className="footer__container">
-                    <label className="footer__message">
-                        {copyrightMessage}
-                    </label>
-                </div>
-            </footer>
-        </>
+        <footer className="footer">
+            <div className="footer__container">
+                <label className="footer__message">{copyrightMessage}</label>
+            </div>
+        </footer>
     );
 }
