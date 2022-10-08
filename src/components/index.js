48bfacd1d9c9080f3492032f0c6e48a0fc2a338d
ndwilliamson123 <ndwilliamson3@gmail.com>
2022-10-08 11:35:03 -0400

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+
+
+//example
+// export { default as Header } from "./Header/Header";
+// export { default as COMPONENT_NAME } from "./COMPONENT_NAME/COMPONENT_NAME";
+
+//then other components just need to reference this 'index.js' file to import all components in this '\components' folder
