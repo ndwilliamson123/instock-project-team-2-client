de509447762e01c8482ef859e83ecbeadd97b55c
Kevin Tran <kevtran@hotmail.com>
2022-10-18 18:25:58 -0700

    frags removed

--- src/components/Buttons/BlueButton/BlueButton.js
+++ src/components/Buttons/BlueButton/BlueButton.js
@@ -2,17 +2,15 @@ import "./BlueButton.scss";
 
 export default function BlueButton({ buttonText, type, image }) {
     return (
-        <>
-            <button type={type} className="blue-button">
-                <img
-                    src={image}
-                    // if no image tag is needed, this wll hide the image tag
+        <button type={type} className="blue-button">
+            <img
+                src={image}
+                // if no image tag is needed, this wll hide the image tag
 
-                    className={!image ? "blue-button__noimage" : null}
-                    alt="buttonpic"
-                ></img>
-                {buttonText}
-            </button>
-        </>
+                className={!image ? "blue-button__noimage" : null}
+                alt="buttonpic"
+            ></img>
+            {buttonText}
+        </button>
     );
 }
