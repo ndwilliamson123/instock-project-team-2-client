4f6c467d01149caa620a8521397a47e1be05185e
Svetlana Davidchuk <stdavidchuk@gmail.com>
2022-10-21 22:32:12 -0400

    header mobile

---
+++ src/components/header/Header.js
@@ -0,0 +1,40 @@
+import "./Header.scss";
+import websiteLogo from "../../assets/logos/InStock-Logo.svg";
+import { NavLink, Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <Link to="/">
+        <div>
+          <img
+            className="header__website-logo"
+            src={websiteLogo}
+            alt="website-logo"
+          />
+        </div>
+      </Link>
+      <nav className="header__nav-bar">
+        <NavLink
+          to="/warehouses"
+          className={(isActive) =>
+            "header__nav-bar-link " +
+            (isActive ? "header__nav-bar-link--active" : "")
+          }
+        >
+          <p>Warehouses</p>
+        </NavLink>
+        <NavLink
+          to="/inventory"
+          className={(isActive) =>
+            "header__nav-bar-link " +
+            (isActive ? "header__nav-bar-link--active" : "")
+          }
+        >
+          <p>Inventory</p>
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -4,20 +4,16 @@ export { default as AddNewWarehouse } from "./AddNewWarehouse/AddNewWarehouse";
 export { default as SelectedWarehouse } from "./SelectedWarehouse/SelectedWarehouse";
 export { default as EditWarehouse } from "./EditWarehouse/EditWarehouse";
 export { default as DeleteWarehouse } from "./DeleteWarehouse/DeleteWarehouse";
-
 export { default as InventoryList } from "./InventoryList/InventoryList";
 export { default as AddNewItem } from "./AddNewItem/AddNewItem";
 export { default as SelectedItem } from "./SelectedItem/SelectedItem";
 export { default as EditItem } from "./EditItem/EditItem";
 export { default as DeleteItem } from "./DeleteItem/DeleteItem";
-
 export { default as BlueButton } from "./Buttons/BlueButton/BlueButton";
 export { default as RedButton } from "./Buttons/RedButton/RedButton";
 export { default as WhiteButton } from "./Buttons/WhiteButton/WhiteButton";
-
 export { default as MainSubHeader } from "./MainSubHeader/MainSubHeader";
 export { default as WarehousesListColumns } from "./WarehousesListColumns/WarehousesListColumns";
-
 export { default as Footer } from "./Footer/Footer";
-
 export { default as AddEditSubheader } from "./AddEditSubheader/AddEditSubheader";
+export { default as Header } from "./header/Header.js";
