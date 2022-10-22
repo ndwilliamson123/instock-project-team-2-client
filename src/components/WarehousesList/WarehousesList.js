c79c0237fe3697c9cf98afa3a7c91b95f8afd9db
Svetlana Davidchuk <stdavidchuk@gmail.com>
2022-10-21 23:48:25 -0400

    forgot to uncomment the alert

--- src/components/WarehousesList/WarehousesList.js
+++ src/components/WarehousesList/WarehousesList.js
@@ -2,56 +2,53 @@ import "./WarehousesList.scss";
 import { Component } from "react";
 import { Route } from "react-router-dom";
 import {
-    DeleteWarehouse,
-    MainSubHeader,
-    WarehousesListColumns,
-    WarehousesListItem,
+  DeleteWarehouse,
+  MainSubHeader,
+  WarehousesListColumns,
+  WarehousesListItem,
 } from "../index";
 import axios from "axios";
 
 export default class WareHousesList extends Component {
-    state = {
-        warehouses: [],
-    };
+  state = {
+    warehouses: [],
+  };
 
-    componentDidMount() {
-        axios
-            .get(`http://localhost:8080/warehouses`)
-            .then((response) => {
-                this.setState({
-                    warehouses: response.data,
-                });
-            })
-            .catch((error) => {
-                console.log(error)
-                // alert(error)
-            });
-    }
+  componentDidMount() {
+    axios
+      .get(`http://localhost:8080/warehouses`)
+      .then((response) => {
+        this.setState({
+          warehouses: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        alert(error);
+      });
+  }
 
-    render() {
-        const { warehouses } = this.state
-        return (
-            <>
-                <Route
-                    path="/warehouses/:warehouseName/delete"
-                    component={(props) => <DeleteWarehouse {...props} />}
-                />
-                <div className="warehouse-list">
-                    <MainSubHeader
-                        title="Warehouses"
-                        buttonText={"+ Add New Warehouse"}
-                    />
-                    <WarehousesListColumns />
-                    <ul className="warehouse-list__list">
-                        {warehouses.map((warehouse) => (
-                            <WarehousesListItem
-                                key={warehouse.id}
-                                warehouse={warehouse}
-                            />
-                        ))}
-                    </ul>
-                </div>
-            </>
-        );
-    }
+  render() {
+    const { warehouses } = this.state;
+    return (
+      <>
+        <Route
+          path="/warehouses/:warehouseName/delete"
+          component={(props) => <DeleteWarehouse {...props} />}
+        />
+        <div className="warehouse-list">
+          <MainSubHeader
+            title="Warehouses"
+            buttonText={"+ Add New Warehouse"}
+          />
+          <WarehousesListColumns />
+          <ul className="warehouse-list__list">
+            {warehouses.map((warehouse) => (
+              <WarehousesListItem key={warehouse.id} warehouse={warehouse} />
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
